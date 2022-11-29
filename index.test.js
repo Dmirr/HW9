27b0c82c94a31baf048a091e23b9ff21510f6eff
Dmirr <dmitriyrodionow@gmail.com>
2022-11-29 21:41:08 +0300

    another test

--- index.test.js
+++ index.test.js
@@ -8,4 +8,10 @@ describe("тестирование расчета бонуса", () => {
     expect(calculateBonus(10, 40)).toBeGreaterThanOrEqual(50);
     expect(calculateBonus(10, 10)).toBeLessThan(50);
   });
+  it("Проверка граничных значений:граница => 50", () => {
+    expect(calculateBonus(51)).toBe(true);
+  });
+  it("Граница < 50", () => {
+    expect(calculateBonus(49)).toBe(false);
+  });
 });
